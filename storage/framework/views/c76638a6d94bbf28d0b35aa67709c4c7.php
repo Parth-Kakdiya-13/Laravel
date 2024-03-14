29e7d3cdf2d2c8ef4433a33ec15fbc08217acb4d
Parth-Kakdiya-13 <parthrkakadiya@gmail.com>
2024-03-14 15:09:51 +0530

    addtomain

---
+++ storage/framework/views/c76638a6d94bbf28d0b35aa67709c4c7.php
@@ -0,0 +1,11 @@
+<?php $__env->startSection('content'); ?>
+    <p>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod distinctio accusamus, quasi veritatis at minus quidem voluptatibus molestiae? Qui voluptas aliquam autem aliquid inventore. Consequuntur officiis exercitationem amet asperiores quasi!</span>
+        <span>Doloremque ab voluptate quibusdam officiis? Quos perspiciatis inventore cum vitae. At a quaerat vitae, sapiente velit quis ipsam dolores aliquid, delectus harum iste! Accusantium nesciunt quos, dolor cum officiis harum?</span>
+        <span>Repellendus iste earum, blanditiis quaerat sunt voluptate incidunt doloribus ipsa asperiores commodi provident quos labore dignissimos molestiae soluta architecto? Velit quas cum, repellendus voluptatibus officia nesciunt eaque tenetur explicabo voluptas.</span>
+        <span>Soluta maiores provident dolor aliquid iste dolore neque repudiandae officia amet dignissimos iusto, dicta sunt eum, dolores, incidunt blanditiis autem placeat eius molestiae rem. Id exercitationem itaque reprehenderit autem iste.</span>
+        <span>Voluptatum debitis sit cum deserunt. Suscipit in rem quis reiciendis reprehenderit laboriosam impedit aliquam quod fugit fuga. Tempora deleniti rem, quibusdam a voluptates, repellendus quia minima impedit ullam in iste.</span>
+    </p>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('templates.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Parth\myapp\resources\views/users/index.blade.php ENDPATH**/ ?>
